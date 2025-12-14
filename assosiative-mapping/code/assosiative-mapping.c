680bd9c730e9b25463f2363bd6da7e5f07a6e62f
Jobin James <jobinjamesjo12@gmail.com>
2025-12-14 12:24:29 +0530

    program changed

--- assosiative-mapping/code/assosiative-mapping.c
+++ assosiative-mapping/code/assosiative-mapping.c
@@ -1,123 +1,93 @@
 #include <stdio.h>
-#include <string.h>
-
-struct Student {
-    char roll[20];
-    char name[50];
-    int mark;
-};
-
-struct Student students[100];
-int count = 0;
-
-void add_student() {
-    printf("Enter Roll Number: ");
-    scanf("%s", students[count].roll);
-
-    printf("Enter Name: ");
-    scanf("%s", students[count].name);
-
-    printf("Enter Mark: ");
-    scanf("%d", &students[count].mark);
-
-    count++;
-    printf("Student added successfully!\n\n");
-}
-
-int find_student(char roll[]) {
-    for (int i = 0; i < count; i++) {
-        if (strcmp(students[i].roll, roll) == 0)
-            return i;
-    }
-    return -1;
-}
-
-void update_mark() {
-    char roll[20];
-    printf("Enter Roll Number to Update: ");
-    scanf("%s", roll);
-
-    int idx = find_student(roll);
-    if (idx != -1) {
-        printf("Enter New Mark: ");
-        scanf("%d", &students[idx].mark);
-        printf("Mark updated!\n\n");
-    } else {
-        printf("Student not found!\n\n");
-    }
-}
-
-void delete_student() {
-    char roll[20];
-    printf("Enter Roll Number to Delete: ");
-    scanf("%s", roll);
-
-    int idx = find_student(roll);
-    if (idx != -1) {
-        for (int i = idx; i < count - 1; i++) {
-            students[i] = students[i + 1];
-        }
-        count--;
-        printf("Student deleted!\n\n");
-    } else {
-        printf("Student not found!\n\n");
-    }
-}
-
-void search_student() {
-    char roll[20];
-    printf("Enter Roll Number to Search: ");
-    scanf("%s", roll);
-
-    int idx = find_student(roll);
-    if (idx != -1) {
-        printf("Name: %s\n", students[idx].name);
-        printf("Mark: %d\n\n", students[idx].mark);
-    } else {
-        printf("Student not found!\n\n");
-    }
-}
-
-void display_all() {
-    if (count == 0) {
-        printf("No students found!\n\n");
-        return;
-    }
-
-    printf("\n---- All Students ----\n");
-    for (int i = 0; i < count; i++) {
-        printf("Roll: %s | Name: %s | Mark: %d\n",
-               students[i].roll,
-               students[i].name,
-               students[i].mark);
-    }
-    printf("\n");
-}
 
 int main() {
-    int choice;
+    int cacheLines, blockSize, n;
+    int cacheTag[20];
+    int address[50];
+    int hits = 0, misses = 0;
+    int filled = 0;
+    int fifoIndex = 0;
+    int i, j;
 
-    while (1) {
-        printf("1. Add Student\n");
-        printf("2. Update Mark\n");
-        printf("3. Delete Student\n");
-        printf("4. Search Student\n");
-        printf("5. Display All Students\n");
-        printf("6. Exit\n");
+    printf("Enter number of cache lines: ");
+    scanf("%d", &cacheLines);
 
-        printf("Enter choice: ");
-        scanf("%d", &choice);
+    printf("Enter block size (words per block): ");
+    scanf("%d", &blockSize);
 
-        switch (choice) {
-            case 1: add_student(); break;
-            case 2: update_mark(); break;
-            case 3: delete_student(); break;
-            case 4: search_student(); break;
-            case 5: display_all(); break;
-            case 6: return 0;
-            default: printf("Invalid choice!\n\n");
-        }
+    printf("Enter number of memory addresses: ");
+    scanf("%d", &n);
+
+    printf("Enter memory addresses:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &address[i]);
     }
+
+    /* Initialize cache */
+    for (i = 0; i < cacheLines; i++) {
+        cacheTag[i] = -1;
+    }
+
+    /* Table Header */
+    printf("\nASSOCIATIVE MAPPING WITH FIFO (WORKING)\n");
+    printf("-------------------------------------------------------------------------------------\n");
+    printf("| Address | TAG | WORD | Hit/Miss | Action (FIFO)     | Cache Contents               |\n");
+    printf("-------------------------------------------------------------------------------------\n");
+
+    for (i = 0; i < n; i++) {
+        int tag = address[i] / blockSize;
+        int word = address[i] % blockSize;
+        int found = 0;
+        char action[25] = "No Change";
+
+        /* Search cache */
+        for (j = 0; j < cacheLines; j++) {
+            if (cacheTag[j] == tag) {
+                hits++;
+                found = 1;
+                break;
+            }
+        }
+
+        /* MISS handling with FIFO */
+        if (!found) {
+            misses++;
+
+            if (filled < cacheLines) {
+                cacheTag[filled] = tag;
+                filled++;
+                sprintf(action, "Inserted");
+            } else {
+                cacheTag[fifoIndex] = tag;
+                sprintf(action, "Replaced line %d", fifoIndex);
+                fifoIndex = (fifoIndex + 1) % cacheLines;
+            }
+        } else {
+            sprintf(action, "Hit (No replace)");
+        }
+
+        /* Print table row */
+        printf("| %7d | %3d | %4d | %-8s | %-18s | ",
+               address[i], tag, word,
+               found ? "HIT" : "MISS",
+               action);
+
+        for (j = 0; j < cacheLines; j++) {
+            if (cacheTag[j] != -1)
+                printf("%d ", cacheTag[j]);
+            else
+                printf("- ");
+        }
+        printf("|\n");
+    }
+
+    printf("-------------------------------------------------------------------------------------\n");
+
+    /* Final results */
+    printf("\nTotal Hits   = %d\n", hits);
+    printf("Total Misses = %d\n", misses);
+    printf("Hit Ratio    = %.2f\n",
+           (float)hits / (hits + misses));
 
     return 0;
 }
