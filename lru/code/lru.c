180fd0aa0fd0db9d9e4bd69c0a9dd3e7595886e1
Jobin James <jobinjamesjo12@gmail.com>
2025-12-14 13:21:43 +0530

    lru code edited

--- lru/code/lru.c
+++ lru/code/lru.c
@@ -3,79 +3,101 @@
 int main() {
     int pages[30], frames[10], time[10];
     int no_of_pages, no_of_frames;
-    int i, j, flag1, flag2, pos, counter = 0, page_faults = 0;
+    int i, j, pos;
+    int counter = 0;
+    int hits = 0, misses = 0;
+    int found, empty;
 
     printf("Enter number of pages: ");
     scanf("%d", &no_of_pages);
 
-    printf("Enter the page sequence: ");
+    printf("Enter the page sequence:\n");
     for (i = 0; i < no_of_pages; i++)
         scanf("%d", &pages[i]);
 
     printf("Enter number of frames: ");
     scanf("%d", &no_of_frames);
 
-    // Initialize frames as empty
+    // Initialize frames
     for (i = 0; i < no_of_frames; i++) {
         frames[i] = -1;
         time[i] = 0;
     }
 
-    printf("\nPage\tFrames\n");
+    // Table header
+    printf("\nLRU PAGE REPLACEMENT (WORKING)\n");
+    printf("-----------------------------------------------------------------\n");
+    printf("| Page | Hit/Miss | Action              | Frames                  |\n");
+    printf("-----------------------------------------------------------------\n");
 
     for (i = 0; i < no_of_pages; i++) {
-        flag1 = flag2 = 0;
+        found = 0;
+        empty = 0;
 
-        // Check if page is already in frame
+        // Check HIT
         for (j = 0; j < no_of_frames; j++) {
             if (frames[j] == pages[i]) {
+                hits++;
                 counter++;
-                time[j] = counter;   // update recent use time
-                flag1 = flag2 = 1;
+                time[j] = counter;
+                found = 1;
                 break;
             }
         }
 
-        // If page not found in frame
-        if (flag1 == 0) {
+        // MISS
+        if (!found) {
+            misses++;
+
+            // Check for empty frame
             for (j = 0; j < no_of_frames; j++) {
-                if (frames[j] == -1) {  // empty frame
-                    counter++;
-                    page_faults++;
+                if (frames[j] == -1) {
                     frames[j] = pages[i];
+                    counter++;
                     time[j] = counter;
-                    flag2 = 1;
+                    empty = 1;
                     break;
                 }
             }
-        }
 
-        // If no empty frame → replace using LRU
-        if (flag2 == 0) {
-            int min = time[0];
-            pos = 0;
+            // Replace using LRU
+            if (!empty) {
+                int min = time[0];
+                pos = 0;
 
-            for (j = 1; j < no_of_frames; j++) {
-                if (time[j] < min) {
-                    min = time[j];
-                    pos = j;
+                for (j = 1; j < no_of_frames; j++) {
+                    if (time[j] < min) {
+                        min = time[j];
+                        pos = j;
+                    }
                 }
-            }
 
-            counter++;
-            page_faults++;
-            frames[pos] = pages[i];
-            time[pos] = counter;
+                frames[pos] = pages[i];
+                counter++;
+                time[pos] = counter;
+            }
         }
 
-        // Print frames status
-        printf("%d\t", pages[i]);
-        for (j = 0; j < no_of_frames; j++)
-            printf("%d ", frames[j]);
-        printf("\n");
+        // Print table row
+        printf("| %4d | %-8s | %-19s | ",
+               pages[i],
+               found ? "HIT" : "MISS",
+               found ? "No change" : (empty ? "Inserted" : "Replaced (LRU)"));
+
+        for (j = 0; j < no_of_frames; j++) {
+            if (frames[j] != -1)
+                printf("%d ", frames[j]);
+            else
+                printf("- ");
+        }
+        printf("|\n");
     }
 
-    printf("\nTotal Page Faults = %d\n", page_faults);
+    printf("-----------------------------------------------------------------\n");
+
+    printf("\nTotal Hits   = %d", hits);
+    printf("\nTotal Misses = %d", misses);
+    printf("\nHit Ratio    = %.2f\n", (float)hits / (hits + misses));
 
     return 0;
 }
